12d8e6fb0c24ef8a28c10955f7ef9070a1f1e513
Lordor5 <thelordor5@gmail.com>
2025-02-16 21:28:59 +0100

    Iniciative terminado

--- InitiativeTracker.js
+++ InitiativeTracker.js
@@ -17,7 +17,10 @@ const { measureBoundingBox } = require("@jscad/modeling").measurements;
 
 //import customFont from "../../vector/CustomFont.js";
 
-const texto = "Enemgo";
+const nozleDiameter = 0.4;
+const layerHeight = 0.2;
+
+const texto = "Enemigo";
 const numero = "1";
 
 const anchoSuperficie = 18; //grosor de la madera/carton
@@ -28,17 +31,15 @@ let magnetHeight = 3; //Grosor del iman
 magnetWidth += 1.4;  //añadimons tolerancias de seguridad
 magnetHeight += 0.4; //añadimons tolerancias de seguridad
 
-const nozleDiameter = 0.4;
-const layerHeight = 0.2;
 const totalWidth = magnetWidth  + 2 * nozleDiameter * 2; //11.4 + 1.6 = 13
-const TotalHeight = magnetHeight + 2 * layerHeight * 2;  //3.4+0.8 = 4.2
+const TotalHeight = magnetHeight + 2 * layerHeight * 4;  //3.4+0.8 = 4.2
 
-const outerRadius = totalWidth/2; // Outer radius of the hexagon
+//const outerRadius = totalWidth/2; // Outer radius of the hexagon
 const raftHeight = 1.2; // Height of the extrusion
 
 const squareThickness = 1.5; // Thickness of the hexagon's wall
 const textBold = 0.4;
-const textWidth = outerRadius / 1.5;
+const textWidth = totalWidth/2 / 1.5;
 const textHeight = 0.5;
 
 module.exports = { main };
@@ -46,23 +47,26 @@ module.exports = { main };
 function main() {
   const enemigo = iniciativa();
 
-  const Veneno = efecto();
+  let veneno = efecto();
+  veneno = translate([0,-15,0],veneno);
 
+  
   //console.log("bounding box: ", measureBoundingBox(Veneno));
-  return [enemigo];
+  
+  return union(veneno,enemigo);
 }
 
 // efectos
 function efecto() {
 
-  
-  let raftBottom = extrudeLinear(
+  /*let raftBottom = extrudeLinear(
     { height: raftHeight },
     polygon({
       points: createArrow(totalWidth/2, 0),
     })
   );
-  raftBottom = rotate([Math.PI / 2, 0,-Math.PI / 2], raftBottom);
+  */
+  
   //raftBottom = translate(
   //  [anchoSuperficie*0, 0, TotalHeight*1.5],
   //  raftBottom
@@ -74,15 +78,14 @@ function efecto() {
   //  magnetHole
   //);
 
-  let raftTop = extrudeLinear(
-    { height: raftHeight },
-    polygon({
-      points: createArrow(totalWidth/2, TotalHeight),
-    })
-  );
+  let raftTop = createArrow(totalWidth/2, 0,raftHeight);
+  raftTop = rotate([Math.PI / 2, 0,Math.PI / 2], raftTop);
+  raftTop = translate([anchoSuperficie/2 + raftHeight/2,-TotalHeight/2,0], raftTop);
+
   
-  console.log("bounding box: ", measureBoundingBox(raftBottom));
-  //raftTop = translateZ(anchoSuperficie + raftHeight, raftTop);
+  let raftBottom = createArrow(totalWidth/2, 0,raftHeight);
+  raftBottom = rotate([Math.PI / 2, 0,Math.PI / 2], raftBottom);
+  raftBottom = translate([-anchoSuperficie/2 - raftHeight/2,-TotalHeight/2,0], raftBottom);
 
   let effect = union( magnetHole, raftBottom);
   //effect = rotate([Math.PI / 2, 0, -Math.PI / 2], effect);
@@ -91,43 +94,56 @@ function efecto() {
   //  effect
   //);
 
-  return effect;
+  return [raftTop, magnetHole, raftBottom];
 }
 
-function createArrow(radius, height) {
+function createArrow(radius,width, height) {
   const points = [];
 
+  radius /= Math.cos(Math.PI / 6);
+  width /= 2;
+
   let angle = (Math.PI / 3) * 0; // 60 degrees for each corner
-  let x = 0;
+  let x = 0 + width;
   let y = 0;
   points.push([x, y]);
 
   angle = (Math.PI / 3) * 1; // 60 degrees for each corner
-  x = radius * Math.cos(angle);
+  x = radius * Math.cos(angle) + width;
   y = radius * Math.sin(angle);
   points.push([x, y]);
 
   angle = (Math.PI / 3) * 2; // 60 degrees for each corner
-  x = radius * Math.cos(angle);
+  x = radius * Math.cos(angle)- width;
   y = radius * Math.sin(angle);
   points.push([x, y]);
 
   angle = (Math.PI / 3) * 3;
-  x = radius * Math.cos(angle);
+  x = radius * Math.cos(angle)- width;
   y = radius * Math.sin(angle);
   points.push([x, y]);
 
   angle = (Math.PI / 3) * 4;
-  x = radius * Math.cos(angle);
+  x = radius * Math.cos(angle)- width;
   y = radius * Math.sin(angle);
   points.push([x, y]);
 
   angle = (Math.PI / 3) * 5;
-  x = radius * Math.cos(angle);
+  x = radius * Math.cos(angle)+ width;
   y = radius * Math.sin(angle);
   points.push([x, y]);
 
-  return points;
+
+  let arrow = extrudeLinear(
+    { height: height },
+    polygon({
+      points: points,
+    })
+  );
+
+  arrow = translate([radius/2,0,-height/2], arrow);
+
+  return arrow;
 }
 
 function createMagnetHole() {
@@ -146,7 +162,7 @@ function createMagnetHole() {
 
   //Make a second hole to be able to take out the magenet
   let magnetExtractor = cuboid({
-    size: [magnetWidth / 3, magnetWidth / 3, magnetHeight],
+    size: [magnetWidth / 4, magnetWidth / 4, magnetHeight],
     center: [0, 0,-magnetWidth / 2],
   });
 
@@ -161,17 +177,21 @@ function createMagnetHole() {
   raftUnion = subtract(raftUnion, magnet);
 
   return [raftUnion];
-
 }
 
 //Iniciativa
 function iniciativa() {
-  const raftTop = createRaft(numero, texto);
+  let raftTop = createRaft(numero, texto);
+  raftTop = rotate([Math.PI / 2,0 , Math.PI / 2], raftTop);
+  raftTop = translate([anchoSuperficie/2,0 , 0], raftTop);
 
-  let raftUnion = extrudeLinear(
+  let raftBottom = createRaft(numero, texto);
+  raftBottom = rotate([-Math.PI / 2,0 , Math.PI / 2], raftBottom);
+  raftBottom = translate([-anchoSuperficie/2,0 , 0], raftBottom);
+  /*let raftUnion = extrudeLinear(
     { height: -anchoSuperficie },
     polygon({
-      points: createHexagonUnion(outerRadius),
+      points: createHexagon(outerRadius,0),
     })
   );
 
@@ -194,125 +214,128 @@ function iniciativa() {
   ); //-outerRadius/10
   magnet = rotate([0, Math.PI / 2, 0], magnet);
 
-  raftUnion = subtract(raftUnion, magnet);
+  
 
   */
   let magnetEntrance = createMagnetHole();
 
-  let raftBottom = createRaft(numero, texto);
-  raftBottom = rotate([/*Math.PI / 100*/0, -Math.PI/2, 0], raftBottom);
-  raftBottom = translate([-anchoSuperficie/2, 0, 0], raftBottom);
+  //let raftBottom = createRaft(numero, texto);
+  //raftBottom = rotate([/*Math.PI / 100*/0, -Math.PI/2, 0], raftBottom);
+  //raftBottom = translate([-anchoSuperficie/2, 0, 0], raftBottom);
 
-  let finalShape = union(raftTop, raftUnion, raftBottom);
+  //let finalShape = union(raftTop, raftUnion);
   //finalShape = rotate([0, -(Math.PI * 2) / 4, 0], finalShape);
   //finalShape = translate([-anchoSuperficie / 2, 20, outerRadius], finalShape);
 
-  return [magnetEntrance,raftBottom];
+  return [magnetEntrance,raftTop,raftBottom];
 }
 
-function createHexagonRaft(radius, width, center) {
-  const points = [];
-  for (let i = 0; i <= 2; i++) {
-    const angle = (Math.PI / 2) * i; // 90? degrees for each corner
-    const x = radius * Math.cos(angle);
-    const y = radius * Math.sin(angle) + center*width/2;
-    points.push([x, y]);
-  }
-  for (let i = 2; i <= 4; i++) {
-    const angle = (Math.PI / 2) * i; // 90? degrees for each corner
-    const x = radius * Math.cos(angle);
-    const y = radius * Math.sin(angle) - center*width/2 - width*(!center);
-    points.push([x, y]);
-  }
-  return points;
-}
-
-function createHexagonUnion(radius) {
+function createHexagon(radius, width, height) {
   const points = [];
 
-  let angle = Math.PI * 2; // 60 degrees for each corner
-  let x = radius * Math.cos(angle);
-  let y = radius * Math.sin(angle) + radius/2;
+  radius /= Math.cos(Math.PI / 6);
+
+  let angle = (Math.PI / 3) * 0; // 60 degrees for each corner
+  let x = radius * Math.cos(angle) + width/2;
+  let y = radius * Math.sin(angle);
   points.push([x, y]);
 
-  angle = Math.PI * 1; // 60 degrees for each corner
-  x = radius * Math.cos(angle);
-  y = radius * Math.sin(angle) + radius/2;
+  angle = (Math.PI / 3) * 1; // 60 degrees for each corner
+  x = radius * Math.cos(angle) + width/2;
+  y = radius * Math.sin(angle);
   points.push([x, y]);
 
-  angle = Math.PI * 1; // 60 degrees for each corner
-  x = radius * Math.cos(angle);
-  y = radius * Math.sin(angle) - radius/2;
+  angle = (Math.PI / 3) * 2; // 60 degrees for each corner
+  x = radius * Math.cos(angle) - width/2;
+  y = radius * Math.sin(angle);
   points.push([x, y]);
 
-  angle = Math.PI * 2;
-  x = radius * Math.cos(angle);
-  y = radius * Math.sin(angle) - radius/2;
+  angle = (Math.PI / 3) * 3;
+  x = radius * Math.cos(angle) - width/2;
+  y = radius * Math.sin(angle);
   points.push([x, y]);
 
-  return points;
+  angle = (Math.PI / 3) * 4;
+  x = radius * Math.cos(angle) - width/2;
+  y = radius * Math.sin(angle);
+  points.push([x, y]);
+
+  angle = (Math.PI / 3) * 5;
+  x = radius * Math.cos(angle) + width/2;
+  y = radius * Math.sin(angle);
+  points.push([x, y]);
+
+
+  let hexagon = extrudeLinear(
+    { height: height },
+    polygon({
+      points: points
+    }),
+  );
+  
+  return hexagon;
 }
 
 function createRaft(numero, text) {
-  // Create outer hexagon
-  const outerHexagon = polygon({
-    points: createHexagonRaft(outerRadius, outerRadius, 1),
-  });
 
-  // Create inner hexagon by reducing the outer radius by the wall thickness
-  const innerHexagon = polygon({
-    points: createHexagonRaft(outerRadius - squareThickness, outerRadius,1),
-  });
-
-  // Subtract inner hexagon from outer hexagon to create the wall
-  let hexagonWithWall = subtract(outerHexagon, innerHexagon);
-  hexagonWithWall = extrudeLinear({ height: textHeight }, hexagonWithWall);
-  hexagonWithWall = translate([0, 0, raftHeight], hexagonWithWall);
 
   //--------------------------------------- Text -----------------------------
 
   let textName = drawText(texto, textWidth);
   textName = extrudeLinear({ height: textHeight }, textName);
-  textName = rotate([0, 0, -(Math.PI * 2) / 4], textName);
+  textName = colorize([1,0,0],textName);
+  //textName = rotate([0, 0, -(Math.PI * 2) / 4], textName);
   textName = translate(
-    [-textWidth / 1.5, -outerRadius * 2, raftHeight],
+    [textWidth*2, -totalWidth/2 / 2, raftHeight],
     textName
   );
 
   // Measure the bounding box of the text to dynamically adjust raft size
   const textBoundingBox = measureBoundingBox(union(textName));
-  const textLength = textBoundingBox[1][1] - textBoundingBox[0][1] + 10; // Height/Length of the text
+  const textLength = textBoundingBox[1][0] - textBoundingBox[0][0]; // Height/Length of the text
 
+  
   let textNumber = drawText(numero, textWidth);
   textNumber = extrudeLinear({ height: textHeight }, textNumber);
   const textNumberBoundingBox = measureBoundingBox(union(textNumber));
-  const textNumberLength =
-    textNumberBoundingBox[1][0] - textNumberBoundingBox[0][1]; // Height/Length of the text
-  //textNumber = rotate([0, 0, -(Math.PI * 2) / 4], textNumber)
+  const textNumberLength = textNumberBoundingBox[1][1] - textNumberBoundingBox[0][1]; // Height/Length of the text
+  textNumber = rotate([0, 0, (Math.PI * 2) / 4], textNumber)
   textNumber = translate(
-    [-textNumberLength / 2, -textWidth / 1.3 - outerRadius / 2, raftHeight],
+    [textNumberLength / 2, -3.3, raftHeight],
     textNumber
   );
+  /*
   if (textNumberLength > outerRadius) {
     textNumber = drawText(numero, textWidth / 1.5);
     textNumber = extrudeLinear({ height: textHeight }, textNumber);
     const textNumberBoundingBox = measureBoundingBox(union(textNumber));
     const textNumberLength =
       textNumberBoundingBox[1][0] - textNumberBoundingBox[0][1]; // Height/Length of the text
-    //textNumber = rotate([0, 0, -(Math.PI * 2) / 4], textNumber)
+    textNumber = rotate([0, 0, (Math.PI * 2) / 4], textNumber)
     textNumber = translate(
-      [-textNumberLength / 2, -textWidth / 2 - outerRadius / 2, raftHeight],
+      [textNumberLength / 4 , -textWidth / 4 - outerRadius / 2, raftHeight],
       textNumber
     );
   }
+*/
+  textNumber = colorize([1,0,0],textNumber);
 
-  const raft = extrudeLinear(
-    { height: raftHeight },
-    polygon({
-      points: createHexagonRaft(outerRadius, textLength + outerRadius,0),
-    })
-  );
-  return union(hexagonWithWall, textName, textNumber, raft);
+
+  //--------  Base Raft ----- Shapes, no text -----
+
+  let outerHexagon = createHexagon(totalWidth/2, 0, textHeight);
+  let innerHexagon = createHexagon(totalWidth/2 - squareThickness, 0, textHeight);
+
+  // Subtract inner hexagon from outer hexagon to create the wall
+  let hexagonWithWall = subtract(outerHexagon, innerHexagon);
+  hexagonWithWall = translate([0,0,raftHeight],hexagonWithWall);
+
+
+  let raft = createHexagon(totalWidth/2, textLength + totalWidth/2, raftHeight);
+  raft = translate([(textLength+ totalWidth/2)/2,0,0],raft);
+  //const ret = union(hexagonWithWall, textNumber, raft);
+  
+  return [textName,hexagonWithWall,raft,textNumber];
 }
 
 function drawText(text, size) {
